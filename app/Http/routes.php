184a451f9be8a396be47f8c36d44227b78738a49
Hanachan1026 <hanachandev@gmail.com>
2015-09-11 16:36:09 +0900

    update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,6 +15,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('files', function() {
+    $dir = '.';
+    $files = Storage::files($dir);
+    return $files;
+});
+
 // App::abort(400, 'BAD');
 // App::abort(500, 'ERROR');
 // // Unauthorized
@@ -26,56 +32,56 @@ Route::get('/', function () {
 // // Not implemented
 // App::abort(501, 'ERROR');
 
-Route::group(array('prefix' => 'api/v1/place'), function() {
-    Route::get('kanto', function() {
-        return array(
-            "Wakashi or Wakanago",
-            "Inada",
-            "Warasa",
-            "Buri"
-        );
-    });
-    Route::post('kansai', function() {
-        echo csrf_field();
-        return Response::json(array('name' => 'Steve', 'state' => 'CA'));
-        // return response()
-        //         ->json(
-        //             ['name' => 'Abigail', 'state' => 'CA']
-        //         );
-    });
-    Route::get('hokuriku', function() {
-        return array(
-            "Tsubaeri",
-            "Kozukura",
-            "Fukuragi",
-            "Aoburi",
-            "Hanajiro",
-            "Buri"
-        );
-    });
-    Route::get('sanin', function() {
-        return array(
-            "Shojigo",
-            "Wakana",
-            "Mejiro",
-            "Hamachi",
-            "Buri"
-        );
-    });
-    Route::get('kyushu', function() {
-        return array(
-            "Wakanago",
-            "Yazu",
-            "Hamachi",
-            "Mejiro",
-            "Buri",
-            "Ouo"
-        );
-    });
-});
-Route::post('foo/bar', function() {
-    return 'hello';
-});
+// Route::group(array('prefix' => 'api/v1/place'), function() {
+//     Route::get('kanto', function() {
+//         return array(
+//             "Wakashi or Wakanago",
+//             "Inada",
+//             "Warasa",
+//             "Buri"
+//         );
+//     });
+//     Route::post('kansai', function() {
+//         echo csrf_field();
+//         return Response::json(array('name' => 'Steve', 'state' => 'CA'));
+//         // return response()
+//         //         ->json(
+//         //             ['name' => 'Abigail', 'state' => 'CA']
+//         //         );
+//     });
+//     Route::get('hokuriku', function() {
+//         return array(
+//             "Tsubaeri",
+//             "Kozukura",
+//             "Fukuragi",
+//             "Aoburi",
+//             "Hanajiro",
+//             "Buri"
+//         );
+//     });
+//     Route::get('sanin', function() {
+//         return array(
+//             "Shojigo",
+//             "Wakana",
+//             "Mejiro",
+//             "Hamachi",
+//             "Buri"
+//         );
+//     });
+//     Route::get('kyushu', function() {
+//         return array(
+//             "Wakanago",
+//             "Yazu",
+//             "Hamachi",
+//             "Mejiro",
+//             "Buri",
+//             "Ouo"
+//         );
+//     });
+// });
+// Route::post('foo/bar', function() {
+//     return 'hello';
+// });
 // Route::group(array('prefix' => 'api/v1/name'), function() {
 //     return array(
 //         "1",
